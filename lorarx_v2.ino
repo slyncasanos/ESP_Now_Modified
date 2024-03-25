bc41dd91059e33548fc7b8531117cadd741b5e2f
slyncasanos <155065797+slyncasanos@users.noreply.github.com>
2024-03-25 18:40:29 +0800

    Update and rename LoRa.h to lorarx_v2.ino

---
+++ lorarx_v2.ino
@@ -0,0 +1,77 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 18
+#define rst 14
+#define dio0 26
+
+typedef struct struct_message {
+  int id;
+  float x;
+  float y;
+  float z;
+}struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Create a structure to hold the readings from each board
+struct_message board1;
+struct_message board2;
+struct_message board3;
+
+// Create an array with all the structures
+struct_message boardsStruct[3] = {board1, board2, board3};
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(866E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+  float board1X = boardsStruct[0].x;
+  float board1Y = boardsStruct[0].y;
+  float board1Z = boardsStruct[0].z;
+  float board2X = boardsStruct[1].x;
+  float board2Y = boardsStruct[1].y;
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
